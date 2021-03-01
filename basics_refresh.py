80c786418b54ec481daafcf545c1b3e348e72d1f
Ron Llewellyn <llewellynron@hotmail.com>
2021-02-28 21:07:13 -0500

    add more files

---
+++ basics_refresh.py
@@ -0,0 +1,38 @@
+#############################
+##Numbers and Math Operators
+#############################
+#
+# a = 9
+# b = 4
+# ## Arithmethic Operators
+# a + b  # addition operator => 13
+# a - b  # substraction operator => 5
+# a * b  # multiplication operator => 36
+# a / b  # division operator => 2.25
+# a // b  # floor division operator => 2
+# 5.0 // 3.0  # => 1.0 # works on floats too
+# a ** b  # exponentiation operator (a to the power of b) => 6561
+# a % b  # modulus operator => 1
+#
+# ## Assignment Operators
+# a = 5
+# a += 2  # Shorthand for a = a + 2 => a = 7
+# a -= 3  # Shorthand for a = a - 3 => a = 4
+# a /= 2  # Shorthand for a = a / 2 => a = 2
+# a *= 3  # Shorthand for a = a * 3 => a = 6
+# a **= 2  # Shorthand for a = a ** 2 => a = 36
+#
+# ## Arithmethic Built-in Function
+# divmod(9, 4)  # returns the quotient and remainder when using floor(integer) division => (2, 1)
+# sum([1, 2, 4])  # returns the sum of an iterable => 7
+# min(1, -2, 3)  # returns the minimum value => -2
+# max(1, 2, 4)  # returns the maximum value => 4
+# a = 10 / 3  # a = 3.3333333333
+# round(a, 4)  # returns a number rounded with 4 decimals => 3.3333
+# pow(2, 4)  # 2 ** 4 = 16
+#
+# mytuple = (1, 2, 4)
+# print(max(mytuple))
+# mymod, myrem = divmod(9, 4)
+# print(f"mymod= {mymod} \t myrem = {myrem}")
+
